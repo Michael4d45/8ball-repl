a37e25a651edcd197727d8162d74c1c9a4f02987
Michael J Harvey <michael4d45@gmail.com>
2020-02-27 23:42:40 -0700

    init

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func getAnswer(text string) string {
+	answers := []string{
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",
+	}
+	return answers[rand.Intn(len(answers))]
+}
+
+func main() {
+	rand.Seed(42)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("I am a Magic 8 ball, ask me a question")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+
+		matched, _ := regexp.MatchString("[A-Z].*\\?$", text)
+		if matched {
+			fmt.Println(getAnswer(text))
+		}
+
+		if strings.Compare("exit", text) == 0 {
+			break
+		}
+	}
+}
